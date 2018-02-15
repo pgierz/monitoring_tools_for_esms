8a8fc66f89a165bfa3172e2572d2e6d834d43a10
Paul Gierz <pgierz@awi.de>
2018-02-15 12:00:56 +0100

    adds ability to get multiple logs at once, and a small script to say who has how many jobs in the queue

--- assess_queue_time.py
+++ assess_queue_time.py
@@ -8,36 +8,45 @@ import argparse
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Tells you how long you are spending in the queue')
-    parser.add_argument('log_file')
+    parser.add_argument('-p', '--plot', action="store_true")
+    parser.add_argument('log_file', nargs="*")
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
-    mpiesm_log_file = args.log_file
-    log_dataframe = pd.read_table(mpiesm_log_file, 
-                              sep=r" :  | -" , 
-                              skiprows=1, 
-                              infer_datetime_format=True,
-                              names=["Date", "Message", "state"], 
-                              engine='python', index_col=0)
-    middle_column = log_dataframe["Message"].apply(lambda x: pd.Series(str(x).split()))
-    log_dataframe.drop("Message", axis=1, inplace=True)
-    middle_column.columns = ["Run Number", "Exp Date", "Job ID"]
-    log_dataframe = pd.concat([log_dataframe, middle_column], axis=1)
-    log_dataframe.set_index(pd.to_datetime(log_dataframe.index), inplace=True)
+    mpiesm_log_files = args.log_file
+    all_means=pd.DataFrame()
+    for log in mpiesm_log_files:
+        log_dataframe = pd.read_table(log, 
+                                      sep=r" :  | -" , 
+                                      skiprows=1, 
+                                      infer_datetime_format=True,
+                                      names=["Date", "Message", "state"], 
+                                      engine='python', index_col=0)
+        middle_column = log_dataframe["Message"].apply(lambda x: pd.Series(str(x).split()))
+        log_dataframe.drop("Message", axis=1, inplace=True)
+        middle_column.columns = ["Run Number", "Exp Date", "Job ID"]
+        log_dataframe = pd.concat([log_dataframe, middle_column], axis=1)
+        log_dataframe.set_index(pd.to_datetime(log_dataframe.index), inplace=True)
 
-    data_dict={log_dataframe.index[i]: 
-           (log_dataframe.index[i+1]-log_dataframe.index[i]).total_seconds()/60.
-           for i in range(len(log_dataframe.index)-1) 
-           if log_dataframe["state"][i] == " done"}
-    time_between_starts_df = pd.DataFrame(data=data_dict, index=["Time between Run"]).transpose()
+        data_dict={log_dataframe.index[i]: (log_dataframe.index[i+1]-log_dataframe.index[i]).total_seconds()/60.
+                   for i in range(len(log_dataframe.index)-1)
+                   if log_dataframe["state"][i] == " done"}
 
-    print(time_between_starts_df.mean())
-    time_between_starts_df.plot(legend=False)
-    plt.ylabel("Time between Model Starts (minutes)")
-    plt.xlabel("Date")
-    plt.show()
+        time_between_starts_df = pd.DataFrame(data=data_dict, index=["Time between Run"]).transpose()
+
+        all_means[log.split("/")[-1].split(".")[0]] = time_between_starts_df.mean()
+
+        if args.plot:
+            time_between_starts_df.plot(legend=False)
+            plt.title(log.split("/")[-1].split(".")[0])
+            plt.ylabel("Time between Model Starts (minutes)")
+            plt.xlabel("Date")
+            plt.hold()
+    all_means["Average"] = all_means.mean(axis=1)
+    print(all_means.transpose())
+    if args.plot: plt.show()
 
 if __name__ == '__main__':
     main()
---
+++ report_users_and_runs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+account=$1
+list_of_users_and_runs=$(squeue -A $account --format='%.18i %.9P %.30j %.8u %.2t %.10M %.6D %R' | tr -s ' ' | cut -d ' ' -f 4,5 | sort | grep -v NAME)
+# echo ${list_of_users_and_runs}
+list_of_unique_users=$(squeue -A $account --format='%.18i %.9P %.30j %.8u %.2t %.10M %.6D %R' | tr -s ' ' | cut -d ' ' -f 5 | sort | grep -v USER | uniq )
+#echo ${list_of_unique_users}
+for user in ${list_of_unique_users}
+do
+    echo "${user} is $(finger ${user} | awk '{printf("%s %s\n", $4, $5);}' | head -n 1)"
+    echo "There are $(squeue -A $account -u ${user} | tr -s ' ' | wc -l) jobs running on the $account queue from this person!"
+    echo -e "\n"
+done
